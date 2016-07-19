c42ba758d8b1b3880820f4b9d799ea831738741f
Ebey John <waytoebeyjohn@gmail.com>
2016-07-19 22:48:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ebeyjohn/login/ActivityUserProfile.java
@@ -0,0 +1,18 @@
+package com.example.ebeyjohn.login;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ActivityUserProfile extends AppCompatActivity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_user_profile);
+
+        textView = (TextView) findViewById(R.id.textViewUsername);
+        Intent intent = getIntent();
+        textView.setText("Welcome User " + intent.getStringExtra(MainActivity.KEY_USERNAME));    }
+}
